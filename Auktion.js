d6fe222aa878d5797c0edbaaa7fab3d6359d3ec5
MikaelFEND17 <Mikael.Larsson2@yh.nackademin.se>
2018-03-15 13:29:15 +0100

    Update

--- Auktion.js
+++ Auktion.js
@@ -201,7 +201,7 @@ class AuctionClass
         this.btnBidSubmit.parentNode.replaceChild(clone, this.btnBidSubmit);
         this.btnBidSubmit = clone;
 
-        auction.FillAuctionCard(this.pID, this.pTitle, this.pDescription, this.pStartDate, this.pEndDate, this.pCountDown, this.pStartingPrice, this.pHighestBid, this.pNumBids, this.btnBidSubmit, this.aLinkShowAllBids, this.countdown);
+        auction.FillAuctionCard(this.pID, this.pTitle, this.pDescription, this.pStartDate, this.pEndDate, this.pCountDown, this.pStartingPrice, this.pHighestBid, this.pNumBids, this.inputBid, this.btnBidSubmit, this.aLinkShowAllBids, this.countdown);
 
     }
 
@@ -264,7 +264,7 @@ class Auction
         this.bids = new Array();
     }
 
-    FillAuctionCard(aIDElement, aTitleElement, aDescElement, aStartDateElement, aEndDateElement, aCoundDownElement, aStartingPriceElement, aHighestBidElement, aNumBidsElement, aBidButton, aShowAllBidsLink , aCountDown)
+    FillAuctionCard(aIDElement, aTitleElement, aDescElement, aStartDateElement, aEndDateElement, aCoundDownElement, aStartingPriceElement, aHighestBidElement, aNumBidsElement, aInputBid, aBidButton, aShowAllBidsLink , aCountDown)
     {
         aIDElement.innerHTML = "<strong>Auktion ID: </strong>" + this.auctionID;
         aTitleElement.innerHTML = "<strong>Titel: </strong>" + this.title;
@@ -298,12 +298,13 @@ class Auction
         if (this.IsExpired())
         {
             aBidButton.disabled = true;
+            aInputBid.disabled = true;
         }
         else
         {
             aBidButton.disabled = false;
+            aInputBid.disabled = false;
         }
-
     }
 
     ClearBids()
@@ -391,6 +392,20 @@ class Auction
             }
 
         }
+    }
+
+    Search()
+    {
+        let searchResult = this.auctions.filter((obj) => obj["titel"].toUpperCase().indexOf(searchWord.toUpperCase()) > -1 || obj["beskrivning"].toUpperCase().indexOf(searchWord.toUpperCase()) > -1); 
+
+        //Rensa listan
+
+        //Fyll på med Sökresultat
+    }
+
+    ShowAllAuctions()
+    {
+        console.log("DO THIS CODE");
     }
 }
 
