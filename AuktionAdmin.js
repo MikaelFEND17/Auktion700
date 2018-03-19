f49a548492d02683831dedb988b3a18f6d4490f3
indiehjaerta <cruel@krulltigern.nu>
2018-03-20 00:56:39 +0100

    Admin updates
    - Added Login

--- AuktionAdmin.js
+++ AuktionAdmin.js
@@ -11,20 +11,22 @@ class AuctionAdmin
 {
     constructor()
     {    
-        this.auctionsURL = "https://nackowskis.azurewebsites.net/api/Auktion/700/";
-        this.bidsURL = "https://nackowskis.azurewebsites.net/api/Bud/700/";
+        this.auctionsURL = "http://nackowskis.azurewebsites.net/api/Auktion/700/";
+        this.bidsURL = "http://nackowskis.azurewebsites.net/api/Bud/700/";
 
         this.auctions = new Array();
 
         this.selectListDelete = document.getElementById("auctions-selectlist");
+
+        this.bidIDs = new Array();
     }
 
     Initialize()
     {
-        let btnCreate = document.getElementById("aution-create");
+        let btnCreate = document.getElementById("auction-button-create");
         btnCreate.addEventListener("click", () => auctionAdmin.CreateAuction() );
 
-        let btnDelete = document.getElementById("aution-delete"); 
+        let btnDelete = document.getElementById("auction-button-delete"); 
         btnDelete.addEventListener("click", () => auctionAdmin.Delete() );
 
         this.LoadAuctions();
@@ -40,6 +42,11 @@ class AuctionAdmin
         let endTime = document.getElementById("auction-timeend"); 
         endTime.value = new Date(dateNow.getTime() - dateNow.getTimezoneOffset() * 60000).toISOString().substring(11, 16);
 
+        let startTimeUpdate = setInterval(function () 
+        {
+            let dateNow = new Date();  
+            startDateTime.value = new Date(dateNow.getTime() - dateNow.getTimezoneOffset() * 60000).toISOString().substring(0, 19);
+        }, 1000);
     }
 
     LoadAuctions()
@@ -140,20 +147,49 @@ class AuctionAdmin
     {
         console.log("Deleting auction ID: " +  this.selectListDelete.value);
 
-        //Ta bort Bud
-        let response1 = await this.DeleteBids(this.selectListDelete.value);
+        let gotBids = await this.GetBids(this.selectListDelete.value);
 
         //Ta bort Auktion
-        let response2 = await this.DeleteAuction(this.selectListDelete.value);
-
-        console.log(response1);
-        console.log(response2);
+        //let response = await this.DeleteAuction(this.selectListDelete.value);
+        //console.log(response);
     }
 
-    async DeleteBids(aID)
+    async GetBids(aID)
+    {
+        fetch(this.bidsURL + aID).then(
+            function (response) 
+            {
+                if (response.status !== 200) 
+                {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status);
+                    return false;
+                }
+
+                response.json().then(
+                     data => this.SaveBids(aID, data)
+                );
+            }.bind(this)
+        ).catch(function (err) 
+        {
+            console.log('Fetch Error :-S', err);
+        });
+    }
+
+    async SaveBids(aID, aData)
+    {
+        this.bidIDs = new Array();
+        for (let bid of aData)
+        {
+            await this.DeleteBid(bid.BudID);
+        }
+
+        this.DeleteAuction(aID);
+    }
+
+    async DeleteBid(aID)
     {
         return fetch(this.bidsURL + aID, {
-                method: 'DELETE'
+                method: 'DELETE',
               });
     }
 
@@ -175,3 +211,57 @@ class AuctionAdmin
 let auctionAdmin = new AuctionAdmin();
 auctionAdmin.Initialize();
 
+class UberSafeLogin
+{
+    constructor()
+    {
+        this.username = "username";
+        this.password = "password";
+
+        this.inputUsername = document.getElementById("auction-username");
+        this.inputPassword = document.getElementById("auction-password"); 
+
+        this.divLogin = document.getElementById("auction-login");
+        this.divCreate = document.getElementById("auction-create");
+        this.divDelete = document.getElementById("auction-delete");
+
+        this.divMessage = document.getElementById("auction-login-message");
+        
+        let btnLogin = document.getElementById("auction-button-login"); 
+        btnLogin.addEventListener("click", () => this.CheckLogin() );
+
+    }
+
+    CheckLogin()
+    {
+        if (this.inputUsername.value === this.username && this.inputPassword.value === this.password)
+        {
+            this.Login();
+        }
+        else
+        {
+            this.divMessage.innerHTML = "Wrong Credentials.";
+        }
+    }
+
+    Login()
+    {
+        this.divLogin.classList.add("hide");
+        this.divCreate.classList.remove("hide");
+        this.divDelete.classList.remove("hide");
+
+        sessionStorage.loggedIn = true;
+    }
+
+    CheckSessionLogin()
+    {
+        if (sessionStorage.loggedIn == "true")
+        {
+            this.Login();
+        }
+    }
+
+}
+
+let login = new UberSafeLogin();
+login.CheckSessionLogin();
